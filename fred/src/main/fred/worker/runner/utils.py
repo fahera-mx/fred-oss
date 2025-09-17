325da16b076c70a8992a0d421895018a0b73009f
rhdzmota <code@rhdzmota.com>
2025-09-16 22:08:32 -0600

    FRD-52 Reusable redis configs retrieval

--- fred/src/main/fred/worker/runner/utils.py
+++ fred/src/main/fred/worker/runner/utils.py
@@ -47,3 +47,28 @@ def get_response_queue_name_from_payload(
         default=None,
         keep=keep,
     )
+
+def get_redis_configs_from_payload(
+        payload: dict,
+        keep: bool = False,
+    ) -> dict:
+    for host_key in ["host", "redis_host"]:
+        if (host := payload.get(host_key) if keep else payload.pop(host_key, None)):
+            break
+    for port_key in ["port", "redis_port"]:
+        if (port := payload.get(port_key) if keep else payload.pop(port_key, None)):
+            break
+    for password_key in ["password", "redis_password"]:
+        if (password := payload.get(password_key) if keep else payload.pop(password_key, None)):
+            break
+    for db_key in ["db", "redis_db"]:
+        if (db := payload.get(db_key) if keep else payload.pop(db_key, None)):
+            break
+    return {
+        "host": host or get_environ_variable(name="REDIS_HOST", default="localhost"),
+        "port": int(port or get_environ_variable(name="REDIS_PORT", default=6379)),
+        "password": password or get_environ_variable(name="REDIS_PASSWORD", default=None),
+        "db": int(db or get_environ_variable(name="REDIS_DB", default=0)),
+        "decode_responses": True,
+        **(payload.get("redis_configs", {}) if keep else payload.pop("redis_configs", {})),
+    }
