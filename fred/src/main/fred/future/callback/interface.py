52c5e14bd4dd8c80e6bf93cd190e1e62bc23d8a7
rhdzmota <code@rhdzmota.com>
2025-09-22 22:31:59 -0600

    FRD-89 Update the callback-interface to allow for both on start/complete

--- fred/src/main/fred/future/callback/interface.py
+++ fred/src/main/fred/future/callback/interface.py
@@ -1,6 +1,8 @@
+from threading import Thread
 from typing import (
     Generic,
     TypeVar,
+    Optional,
 )
 
 from fred.settings import logger_manager
@@ -13,10 +15,13 @@ A = TypeVar("A")
 
 class CallbackInterface(Generic[A]):
     
-    def execute(self, future_id: str, output: EitherMonad.Either[A]):
-        raise NotImplementedError()
+    def _on_start(self, future_id: str):
+        raise NotImplementedError
     
-    def run(self, future_id: str, output: EitherMonad.Either[A]) -> bool:
+    def _on_complete(self, future_id: str, output: EitherMonad.Either[A]):
+        raise NotImplementedError
+    
+    def run(self, future_id: str, blocking: bool = False, output: Optional[EitherMonad.Either[A]] = None) -> Thread:
         """Executes the callback with the provided output and handles any exceptions.
         Args:
             output (EitherMonad.Either[A]): The output to be passed to the callback.
@@ -26,8 +31,21 @@ class CallbackInterface(Generic[A]):
         # TODO: Consider using a richer return type to capture more details about the execution
         #  and optionally propagate the callback return value.
         try:
-            self.execute(future_id=future_id, output=output)
-            return True
+            match output:
+                case None:
+                    thread = Thread(
+                        target=lambda: self._on_start(future_id=future_id),
+                        daemon=True
+                    )
+                case EitherMonad.Either():
+                    thread = Thread(
+                        target=lambda: self._on_complete(future_id=future_id, output=output),
+                        daemon=True
+                    )
+            thread.start()
+            if blocking:
+                thread.join()
+            return thread
         except Exception as e:
             logger.error(f"Callback execution failed on future '{future_id}': {e}")
-            return False
+            return None
