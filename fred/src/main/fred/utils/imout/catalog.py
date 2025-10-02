1cecc17cd75755abdd64cd1891da337da09554c8
rhdzmota <code@rhdzmota.com>
2025-10-02 00:20:49 -0600

    FRD-142 Register minio implementation in the catalog

--- fred/src/main/fred/utils/imout/catalog.py
+++ fred/src/main/fred/utils/imout/catalog.py
@@ -5,12 +5,14 @@ from PIL.Image import Image
 from fred.utils.imout.interface import ImageOutputInterface
 from fred.utils.imout._filesystem import OutputFilesystem
 from fred.utils.imout._string import OutputString
+from fred.utils.imout._minio import OutputMinio
 
 
 class ImageOutputCatalog(enum.Enum):
     B64 = OutputString
     STRING = OutputString
-    FILESYSTEM = OutputFilesystem 
+    FILESYSTEM = OutputFilesystem
+    MINIO = OutputMinio
 
     def __call__(self, image: Image, **kwargs) -> ImageOutputInterface:
         if getattr(self.value, "auto", None):
