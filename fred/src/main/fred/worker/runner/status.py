0d25344c1f79a641eaed08c6cf1f67879c731fde
rhdzmota <code@rhdzmota.com>
2025-10-03 21:38:53 -0600

    FRD-152 Standardize key-name and include queue-slug as the prefix value (all runners require a queue-slug)

--- fred/src/main/fred/worker/runner/status.py
+++ fred/src/main/fred/worker/runner/status.py
@@ -14,14 +14,14 @@ class RunnerStatus(Enum):
     @staticmethod
     def get_key(runner_id: str) -> str:
         """Get the status key for the specified runner ID."""
-        return f"runner:status:{runner_id}"
+        return f"frd:runner:{runner_id}"
     
-    def get_val(self, *include) -> str:
+    def get_val(self, queue_slug: str, *include) -> str:
         """Get the string representation of the status."""
-        return ":".join([self.name, datetime_utcnow().isoformat(), *include]) 
+        return ":".join([queue_slug, self.name, datetime_utcnow().isoformat(), *include]) 
     
     @classmethod
-    def parse_value(cls, value: str) -> "RunnerStatus":
+    def parse_value(cls, value: str) -> tuple[str, "RunnerStatus"]:
         """Parse the status from the stored value."""
-        val, *_ = value.split(":")
-        return cls[val.upper()]
+        queue_slug, status, *_ = value.split(":")
+        return (queue_slug, cls[status.upper()])
