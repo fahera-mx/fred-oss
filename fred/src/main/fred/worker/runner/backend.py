f5e12e76da5d0a3181dddf834abb25f37bd65946
rhdzmota <code@rhdzmota.com>
2025-10-03 21:47:05 -0600

    FRD-152 Keep reference of both service instance and catalog (this should be revisited eventually)

--- fred/src/main/fred/worker/runner/backend.py
+++ fred/src/main/fred/worker/runner/backend.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 
 from fred.settings import logger_manager
-from fred.dao.comp.catalog import FredKeyVal, FredQueue
+from fred.dao.comp.catalog import FredKeyVal, FredQueue, CompCatalog
+from fred.dao.service.interface import ServiceInterface
 from fred.dao.service.catalog import ServiceCatalog
 
 logger = logger_manager.get_logger(name=__name__)
@@ -11,23 +12,31 @@ logger = logger_manager.get_logger(name=__name__)
 class RunnerBackend:
     keyval: FredKeyVal
     queue: FredQueue
-    _cat: ServiceCatalog
+    # NOTE: Catalog and service instance references (for internal use)
+    # should be a temporal fix until we refactor the backend system
+    # according to a more precise usage-patter (still to be defined/identified).
+    _cat: ServiceCatalog  # Contains complementary service info and simple references
+    _srv: ServiceInterface  # Allows direct access to a client instance
 
     @classmethod
     def auto(cls, service_name: str, **kwargs) -> 'RunnerBackend':
-        service = ServiceCatalog[service_name.upper()]
-        match service:
+        match (srv_catalog := ServiceCatalog[service_name.upper()]):
             case ServiceCatalog.REDIS:
                 from fred.dao.service.utils import get_redis_configs_from_payload
                 service_kwargs = get_redis_configs_from_payload(kwargs)
             case ServiceCatalog.STDLIB:
                 service_kwargs = {}
             case _:
-                logger.error(f"Unknown service '{service_name}'... will attempt to use provided kwargs as-is.")
+                logger.error(
+                    f"Unknown service '{service_name}'... "
+                    "will attempt to use provided kwargs as-is."
+                )
                 service_kwargs = kwargs
-        components = service.component_catalog(**service_kwargs)
+        logger.info(f"Initializing RunnerBackend using service '{service_name}'")
+        srv_instance = srv_catalog.auto(**service_kwargs)
         return cls(
-            keyval=components.KEYVAL.value,
-            queue=components.QUEUE.value,
-            _cat=service,
+            keyval=CompCatalog.KEYVAL.value.mount(srv_ref=srv_instance),
+            queue=CompCatalog.QUEUE.value.mount(srv_ref=srv_instance),
+            _cat=srv_catalog,
+            _srv=srv_instance,
         )
