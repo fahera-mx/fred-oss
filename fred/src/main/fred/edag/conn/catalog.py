ceff11685913a9bf42199294c3851c39ca3d5ded
rhdzmota <code@rhdzmota.com>
2025-10-20 19:40:10 -0600

    FRD-175 Passthrough connector should run inplace

--- fred/src/main/fred/edag/conn/catalog.py
+++ fred/src/main/fred/edag/conn/catalog.py
@@ -8,11 +8,13 @@ class ConnCatalog(enum.Enum):
         name="conn:passthrough",
         function=lambda **kwargs: kwargs,
         explode=True,
+        inplace=True,
     )
     PASS_PRINT = CompCatalog.NODE.ref.auto(
         name="conn:passthrough_print",
         function=lambda **kwargs: print(kwargs) or kwargs,
         explode=True,
+        inplace=True,
     )
 
     def __call__(self, *args, **kwargs) -> CompCatalog.NODE.ref:
